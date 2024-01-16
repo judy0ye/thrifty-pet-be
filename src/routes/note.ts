2e008a37a659ca8a834377d897b338934ecc56ea
Judy Ye <judyy.ye@gmail.com>
2024-01-16 15:28:36 -0800

    Change naming of routes

--- src/routes/note.ts
+++ src/routes/note.ts
@@ -3,10 +3,10 @@ import controller from '../controllers/note'
 
 const router = Router()
 
-router.post('/', controller.createNote);
-router.get('/', controller.getAllNotes);
-router.get('/:noteId', controller.getNoteById);
-router.patch('/:noteId', controller.updateNote);
-router.delete('/:noteId', controller.deleteNote);
+router.post('/create', controller.createNote);
+router.get('/get', controller.getAllNotes);
+router.get('/get/:noteId', controller.getNoteById);
+router.patch('/update/:noteId', controller.updateNote);
+router.delete('/delete/:noteId', controller.deleteNote);
 
 export default router
